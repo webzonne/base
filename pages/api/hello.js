aef2f9dfcb20fc4b0d10c3f174f507177dbd4d3d
hector silva <hectorsf21@gmail.com>
2023-04-06 17:28:39 -0400

    new

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+export default async function handler(req, res) {
+  // res.status(200).json({ name: 'John Doe' })
+  const {method} = req
+  if(method==='POST'){
+    try{
+      const datos = await req.body
+      console.log(datos)
+      res.status(200).json({ status:'success'})
+    }catch(err){
+        console.log(err)
+        console.log('ocurrio un error')
+    }
+  }
 }
 
-export const config = {
-  api: {
-    bodyParser: false, // Disallow body parsing, consume as stream
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false, // Disallow body parsing, consume as stream
+//   },
+// };
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import axios from 'axios'
 export default function Home() {
+
+  const initial={
+    nombre:''
+  }
+  const [data,setdata]=useState(initial)
+
   const {push} = useRouter()
+
+  const handleChange = (e)=>{
+    const {name, value} = e.target
+    setdata({...data, [name]:value})
+  }
   const submit = (e)=>{
     e.preventDefault()
-    push('/api/hello')
+    const formData = new FormData()
+    formData.append('nombre',data.nombre)
+    try{
+      axios.post('/api/hello',formData,{ 
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+    }catch(err){
+      console.log(err)
+    }
+    
+    push('/')
   }
   return (
     <>
      <h1>HOLA MUNDO</h1>
     <form onSubmit={submit}>
+        <input onChange={handleChange} type='text' name='nombre'/>
         <button type='submit'>ENVIAR</button>
     </form>
     </>
