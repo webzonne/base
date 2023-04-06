19953eda97b267080d9f9bba150fce89c3b80112
hector silva <hectorsf21@gmail.com>
2023-04-06 16:57:40 -0400

    4

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import { useRouter } from 'next/router';
 export default function Home() {
+  const {push} = useRouter()
+  const submit = (e)=>{
+    e.preventDefault()
+    push('/api/hello')
+  }
   return (
     <>
      <h1>HOLA MUNDO</h1>
-    <form action='/api/hellow'>
+    <form onSubmit={submit}>
         <button type='submit'>ENVIAR</button>
     </form>
     </>
