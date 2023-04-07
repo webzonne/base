528c9f973429df1a44aef26625bb5930fba09437
hector silva <hectorsf21@gmail.com>
2023-04-06 20:41:12 -0400

    new add base64

--- pages/index.js
+++ pages/index.js
@@ -2,50 +2,81 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import axios from 'axios'
 import FileBase from "react-file-base64";
-export default function Home() {
+import modelo from "../model/modelo";
+import db from '../lib/db';
+import Image from 'next/image'
 
-  const initial={
-    nombre:'',
-    image:''
-  }
-  const [data,setdata]=useState(initial)
+// SERVER
+export async function getServerSideProps() {
 
-  const {push} = useRouter()
 
-  const handleChange = (e)=>{
-    const {name, value} = e.target
-    setdata({...data, [name]:value})
+  try {
+    await db()
+    const respuestaBase = await modelo.find({})
+    const respuesta = respuestaBase.map((e) => {
+      const res = e.toObject()
+      res._id = res._id.toString()
+      return res
+    })
+    return { props: { respuesta } }
+  } catch (err) {
+    console.log(err)
+    return { props: { respuesta: [] } }
   }
-  const handleFileChange = (file)=>{
-    setdata({...data, image:file.base64})
+
+}
+// SERVER FINAL
+
+
+
+export default function Home({ respuesta }) {
+  console.log(respuesta)
+  const initial = {
+    nombre: '',
+    image: ''
   }
-  const submit = (e)=>{
+  const [data, setdata] = useState(initial)
+
+  const { push } = useRouter()
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setdata({ ...data, [name]: value })
+  }
+  const handleFileChange = (file) => {
+    setdata({ ...data, image: file.base64 })
+  }
+  const submit = (e) => {
     e.preventDefault()
 
-      axios.post('/api/hello',data,{ 
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res=>{
-        console.log(res.data)
-      }).catch(err=>{
-        console.log(err)
-      })
+    axios.post('/api/hello', data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      console.log(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
     push('/')
   }
   return (
     <>
-     <h1>HOLA MUNDO</h1>
-    <form onSubmit={submit}>
-        <input onChange={handleChange} type='text' name='nombre'/><br/><br/>
+      <h1>HOLA MUNDO</h1>
+      <form onSubmit={submit}>
+        <input onChange={handleChange} type='text' name='nombre' /><br /><br />
         <FileBase
-            type="file"
-            multiple={false}
-            onDone={handleFileChange}
-          /><br/><br/>
+          type="file"
+          multiple={false}
+          onDone={handleFileChange}
+        /><br /><br />
         <button type='submit'>ENVIAR</button>
-    </form>
+      </form><br /><br />
+      <h1>RESULTADO</h1>
+      {respuesta &&
+      <img src={respuesta[1].image}></img>
+       }
     </>
-   
+
   )
 }
